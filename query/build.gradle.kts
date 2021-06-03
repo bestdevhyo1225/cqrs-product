2fa2b9dd54333cf1269daaa210d17f9b6002292d
janghyoseog <bestdevhyo1225@gmail.com>
2021-06-04 00:51:37 +0900

    feat: Query 모듈 추가

---
+++ query/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    id("org.springframework.boot")
+
+    kotlin("jvm")
+    kotlin("plugin.spring")
+    kotlin("plugin.jpa")
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-amqp")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+}
