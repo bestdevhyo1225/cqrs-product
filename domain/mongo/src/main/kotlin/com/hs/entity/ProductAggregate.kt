92e9ea07c400f167828a5dff8b868f97d53a6d53
janghyoseog <bestdevhyo1225@gmail.com>
2021-06-21 15:49:51 +0900

    feat: ProductAggregate에 상품 진열 여부, 생성일, 수정일 필드 추가
    [#9]

--- domain/mongo/src/main/kotlin/com/hs/entity/ProductAggregate.kt
+++ domain/mongo/src/main/kotlin/com/hs/entity/ProductAggregate.kt
@@ -8,14 +8,20 @@ import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.index.CompoundIndex
 import org.springframework.data.mongodb.core.index.CompoundIndexes
 import org.springframework.data.mongodb.core.mapping.Document
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 @Document(collection = "product_aggregates")
 @CompoundIndexes(
     value = [
-        CompoundIndex(name = "PRODUCT_AGGREGATES_IX_SEARCH", def = "{'productId': 1, 'type': 1}", unique = true)
+        CompoundIndex(
+            name = "PRODUCT_AGGREGATES_IX_SEARCH",
+            def = "{'productId': 1, 'type': 1, 'isDisplay': 1}",
+            unique = true
+        )
     ]
 )
-class ProductAggregate(productId: Long, type: ProductAggregateType, data: FindProductAggregateDto) {
+class ProductAggregate(productId: Long, type: ProductAggregateType, isDisplay: Boolean, data: FindProductAggregateDto) {
 
     @Id
     var id: String? = null
@@ -27,7 +33,16 @@ class ProductAggregate(productId: Long, type: ProductAggregateType, data: FindPr
     var type: ProductAggregateType = type
         protected set
 
+    var isDisplay: Boolean = isDisplay
+        protected set
+
     var data: FindProductAggregateDto = data
+        protected set
+
+    var createdDatetime: String = LocalDateTime.now().format(DATETIME_FORMATTER).toString()
+        protected set
+
+    var updatedDatetime: String = LocalDateTime.now().format(DATETIME_FORMATTER).toString()
         protected set
 
     override fun toString() = kotlinToString(properties = toStringProperties)
@@ -35,18 +50,23 @@ class ProductAggregate(productId: Long, type: ProductAggregateType, data: FindPr
     override fun hashCode() = kotlinHashCode(properties = equalsAndHashCodeProperties)
 
     companion object {
+        private val DATETIME_FORMATTER: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
         private val equalsAndHashCodeProperties = arrayOf(ProductAggregate::id)
         private val toStringProperties = arrayOf(
             ProductAggregate::id,
             ProductAggregate::productId,
             ProductAggregate::type,
+            ProductAggregate::isDisplay,
             ProductAggregate::data,
+            ProductAggregate::createdDatetime,
+            ProductAggregate::updatedDatetime,
         )
 
         fun create(productAggregateDto: FindProductAggregateDto, type: ProductAggregateType): ProductAggregate {
             return ProductAggregate(
                 productId = productAggregateDto.productId,
                 type = type,
+                isDisplay = productAggregateDto.confirmStatus != "WAIT",
                 data = productAggregateDto
             )
         }
@@ -54,5 +74,6 @@ class ProductAggregate(productId: Long, type: ProductAggregateType, data: FindPr
 
     fun changeProductAggregateData(data: FindProductAggregateDto) {
         this.data = data
+        this.updatedDatetime = LocalDateTime.now().format(DATETIME_FORMATTER).toString()
     }
 }
