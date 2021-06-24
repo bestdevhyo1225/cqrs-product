314dba08579bc7055daecf6b20fc4573d283f4e9
janghyoseog <bestdevhyo1225@gmail.com>
2021-06-24 23:22:31 +0900

    fix: 이벤트 로그 쌓이지 않는 버그 수정

--- command/src/main/kotlin/com/hs/application/usecase/PublishedEventLogCommand.kt
+++ command/src/main/kotlin/com/hs/application/usecase/PublishedEventLogCommand.kt
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
-@Transactional
 class PublishedEventLogCommand(
     private val publishedEventLogRepository: PublishedEventLogRepository
 ) {
