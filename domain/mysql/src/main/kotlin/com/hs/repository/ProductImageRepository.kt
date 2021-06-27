962502c5c8ab51307a491715fa1517b1d221c6ce
janghyoseog <bestdevhyo1225@gmail.com>
2021-06-28 02:19:22 +0900

    style: @Modifying 주석 추가

--- domain/mysql/src/main/kotlin/com/hs/repository/ProductImageRepository.kt
+++ domain/mysql/src/main/kotlin/com/hs/repository/ProductImageRepository.kt
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.Query
 interface ProductImageRepository : JpaRepository<ProductImage, Long> {
 
     /*
-    * @Modifying은 수정, 삭제애서 벌크 연산시 추가해야 하는 어노테이션이다.
+    * @Modifying은 INSERT, UPDATE, DELETE 쿼리에서 사용하고, 주로 벌크 연산시 적용하는 어노테이션이다.
     * - clearAutomatically 옵션은 영속성 컨텍스트를 Clear 할 것인지를 지정하는 속성이다.
     * */
     @Modifying
