f441e45b08393b64eba074baa85a37c7840ed9ed
janghyoseog <bestdevhyo1225@gmail.com>
2021-07-25 03:35:53 +0900

    refactor: Query App Exception 메시지 삭제

--- common/src/main/kotlin/com/hs/message/QueryAppExceptionMessage.kt
+++ common/src/main/kotlin/com/hs/message/QueryAppExceptionMessage.kt
@@ -1,6 +1,5 @@
 package com.hs.message
 
 enum class QueryAppExceptionMessage(val localizedMessage: String) {
-    NOT_FOUND_PRODUCT("[Query - Application] 해당 상품이 존재하지 않습니다. 상품 번호를 다시 확인해주세요!"),
-    NOT_FOUND_PRODUCT_BY_CREATE_OR_UPDATE("[Query - Application] 해당 상품은 현재 확인할 수 없는 상태입니다. (생성 or 수정된 상태)"),
+    NOT_FOUND_PRODUCT("[Query - Application] 해당 상품이 존재하지 않습니다. 상품 번호를 다시 확인해주세요!")
 }
