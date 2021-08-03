ce235f7a7f642da73c6785dd2de674e6075b3f5c
janghyoseog <bestdevhyo1225@gmail.com>
2021-08-04 06:31:30 +0900

    refactor: 파라미터 값 변경

--- domain/mysql/src/main/kotlin/com/hs/service/ProductQueuePublisher.kt
+++ domain/mysql/src/main/kotlin/com/hs/service/ProductQueuePublisher.kt
@@ -1,7 +1,5 @@
 package com.hs.service
 
-import com.hs.dto.PublishProductDto
-
 interface ProductQueuePublisher {
-    fun publish(publishProductDto: PublishProductDto)
+    fun publish(productId: Long)
 }
