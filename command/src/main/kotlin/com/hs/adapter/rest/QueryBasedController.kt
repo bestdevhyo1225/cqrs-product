8fe1c365c9c2e18d22eb077cf44d305139c295b3
janghyoseog <bestdevhyo1225@gmail.com>
2021-08-24 23:11:44 +0900

    style: request root path 수정

--- command/src/main/kotlin/com/hs/adapter/rest/QueryBasedController.kt
+++ command/src/main/kotlin/com/hs/adapter/rest/QueryBasedController.kt
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping(value = ["/query-based"])
+@RequestMapping(value = ["/query"])
 class QueryBasedController(private val productQueryBasedHandler: ProductQueryBasedHandler) {
 
     @GetMapping(value = ["/products/{id}"])
