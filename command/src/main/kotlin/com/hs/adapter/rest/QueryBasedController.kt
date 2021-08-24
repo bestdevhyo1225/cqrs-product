f01b1bceba6ed34b2fa11753d069e2b84371c0f2
janghyoseog <bestdevhyo1225@gmail.com>
2021-08-24 23:08:09 +0900

    feat: 순수 쿼리 용도의 Controller 기능 추가

--- command/src/main/kotlin/com/hs/adapter/rest/QueryBasedController.kt
+++ command/src/main/kotlin/com/hs/adapter/rest/QueryBasedController.kt
@@ -1,9 +1,21 @@
 package com.hs.adapter.rest
 
+import com.hs.application.usecase.QueryBasedHandler
+import com.hs.dto.FindProductDto
+import com.hs.response.SuccessResponse
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping(value = ["/query-based"])
-class QueryBasedController {
+class QueryBasedController(private val queryBasedHandler: QueryBasedHandler) {
+
+    @GetMapping(value = ["/products/{id}"])
+    fun findProduct(@PathVariable(value = "id") productId: Long): ResponseEntity<SuccessResponse<FindProductDto>> {
+        val product: FindProductDto = queryBasedHandler.findProduct(id = productId)
+        return ResponseEntity.ok(SuccessResponse(data = product))
+    }
 }
