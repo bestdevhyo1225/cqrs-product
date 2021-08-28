7ab17d02b029b6ad1be5df0074899ba1059eb0e2
janghyoseog <bestdevhyo1225@gmail.com>
2021-08-28 16:27:22 +0900

    refactor: 페이지네이션 Dto 클래스 변경

---
+++ common/src/main/kotlin/com/hs/dto/FindProductAggregatePaginationDto.kt
@@ -0,0 +1,16 @@
+package com.hs.dto
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.JsonTypeInfo
+
+@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS)
+data class FindProductAggregatePaginationDto(
+    @JsonProperty("productId")
+    val productId: Long,
+
+    @JsonProperty("name")
+    val name: String,
+
+    @JsonProperty("price")
+    val price: Int,
+)
