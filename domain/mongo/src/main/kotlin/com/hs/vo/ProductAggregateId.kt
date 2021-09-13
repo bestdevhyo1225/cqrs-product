7c930deaa67aef612e5407776ddbd9cb74206fef
janghyoseog <bestdevhyo1225@gmail.com>
2021-09-13 23:32:20 +0900

    comment: 정적 팩토리 메서드 패턴, 일급 컬렉션 개념 주석 추가

--- domain/mongo/src/main/kotlin/com/hs/vo/ProductAggregateId.kt
+++ domain/mongo/src/main/kotlin/com/hs/vo/ProductAggregateId.kt
@@ -3,6 +3,24 @@ package com.hs.vo
 import com.hs.exception.DomainMongoException
 import com.hs.exception.DomainMongoExceptionMessage
 
+/*
+* [ 정적 팩토리 메서드 패턴 ]
+* - 디자인 패턴의 팩토리 메서드 패턴과는 다른 패턴이다. 이름만 비슷하다.
+* - 클래스가 불변임을 보장하려면, 자신을 상속하지 못하게 해야한다. 이것을 간단하게 하려면, final 클래스로 선언할 수 있지만,
+*   이보다 더 유연한 방법이 정적 팩토리 메서드 패턴이다.
+*
+* 장점)
+* - 이름을 가질 수 있다.
+* - 호출될 때 마다 인스턴스를 새로 생성하지 않아도 된다. (인스턴스의 변수에 따라 캐싱 전략을 사용할 수 있다.)
+* - 반환 타입의 하위 타입 객체를 반환할 수 있는 능력이 있다.
+* - 입력 매개변수에 따라 매번 다른 클래스의 객체를 반환할 수 있다.
+* - 정적 팩터리 메서드를 작성하는 시점에는 반환할 객체의 클래스가 존재하지 않아도 된다.
+*
+* 단점)
+* - 상속을 하려면, public, protected 생성자가 필요하니, 정적 팩터리 메서드만 제공하면, 하위 클래스를 만들 수 없다.
+* - 정적 팩터리 메서드는 프로그래머가 찾기 어렵다.
+* */
+
 class ProductAggregateId private constructor(
     private var id: String?,
     private val productId: Long
--- domain/mongo/src/main/kotlin/com/hs/vo/ProductImageUrls.kt
+++ domain/mongo/src/main/kotlin/com/hs/vo/ProductImageUrls.kt
@@ -1,5 +1,20 @@
 package com.hs.vo
 
+/*
+* [ 일급 컬렉션 ]
+* - Collection을 Wrapping 하면서, Wrapping한 Collection 외에 다른 멤버 변수가 없다는 것이 특징이다.
+*
+* 장점)
+* - 하나의 인스턴스에서 비즈니스 로직을 관리할 수 있다.
+*   ex) 만약에 urls 길이가 50이 넘어가는 것은 제외해야 하는 경우
+* - 리스트 내의 객체 상태를 동일하게 관리할 수 있다.
+*   - 일급 컬렉션이 불변성을 보장한다는 것은 아니다.
+*   - 일급 컬렉션은 불변성을 보장하지 않으며, 보장하도록 구현해야 할 필요도 없다.
+*
+* 참고)
+* - 일급 컬렉션의 비즈니스 로직에 의해서 상태가 변경되어도 괜찮다. (불변 클래스가 아니어도 되기 때문에)
+* */
+
 class ProductImageUrls private constructor(
     private val productImageUrls: List<String>
 ) {
@@ -16,4 +31,8 @@ class ProductImageUrls private constructor(
     }
 
     fun getProductImageUrls(): List<String> = productImageUrls
+
+    fun getImageUrlsWithLengthLessThanFifty(): List<String> {
+        return productImageUrls.filter { it.length < 50 }
+    }
 }
