2cbffd458720463174a1b340fe444bbf2201cc2d
janghyoseog <bestdevhyo1225@gmail.com>
2022-07-19 23:10:55 +0900

    style: 싱글턴 객체 스타일로 변경

--- common/src/main/kotlin/com/hs/util/ParameterizedTypeReferenceUtils.kt
+++ common/src/main/kotlin/com/hs/util/ParameterizedTypeReferenceUtils.kt
@@ -2,10 +2,8 @@ package com.hs.util
 
 import org.springframework.core.ParameterizedTypeReference
 
-class ParameterizedTypeReferenceUtils {
-    companion object {
-        inline fun <reified T : Any> typeRef(): ParameterizedTypeReference<T> {
-            return object : ParameterizedTypeReference<T>() {}
-        }
+object ParameterizedTypeReferenceUtils {
+    inline fun <reified T : Any> typeRef(): ParameterizedTypeReference<T> {
+        return object : ParameterizedTypeReference<T>() {}
     }
 }
